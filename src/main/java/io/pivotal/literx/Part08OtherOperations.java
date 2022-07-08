ad7b86137beb4ab20007df8fb506f59c4609cdd7
Steven Calderon <scalderon1504@gmail.com>
2022-07-08 17:38:04 -0500

    Solucion parcial ejercicio 8

--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -5,6 +5,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Learn how to use various other operators.
@@ -15,51 +17,55 @@ public class Part08OtherOperations {
 
 //========================================================================================
 
-	// TODO Create a Flux of user from Flux of username, firstname and lastname.
-	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
-		return null;
-	}
+    // Create a Flux of user from Flux of username, firstname and lastname.
+    Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
+        return Flux
+                .zip(usernameFlux, firstnameFlux, lastnameFlux, firstnameFlux)
+                .map(objects -> new User(objects.getT1(), objects.getT2(), objects.getT3()));
+    }
 
 //========================================================================================
 
-	// TODO Return the mono which returns its value faster
-	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
-	}
+    // Return the mono which returns its value faster
+    Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
+        return mono1
+                .mergeWith(mono2)
+                .next();
+    }
 
 //========================================================================================
 
-	// TODO Return the flux which returns the first value faster
-	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
-		return null;
-	}
+    // TODO: Return the flux which returns the first value faster
+    Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
+        return flux1.mergeWith(flux2).take(1);
+    }
 
 //========================================================================================
 
-	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
-	Mono<Void> fluxCompletion(Flux<User> flux) {
-		return null;
-	}
+    // Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
+    Mono<Void> fluxCompletion(Flux<User> flux) {
+        return flux.then();
+    }
 
 //========================================================================================
 
-	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
-	Mono<User> nullAwareUserToMono(User user) {
-		return null;
-	}
+    // Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
+    Mono<User> nullAwareUserToMono(User user) {
+        return (Objects.isNull(user)) ? Mono.empty() : Mono.just(user);
+    }
 
 //========================================================================================
 
-	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
-	Mono<User> emptyToSkyler(Mono<User> mono) {
-		return null;
-	}
+    // Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
+    Mono<User> emptyToSkyler(Mono<User> mono) {
+        return mono.defaultIfEmpty(User.SKYLER);
+    }
 
 //========================================================================================
 
-	// TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
-	Mono<List<User>> fluxCollection(Flux<User> flux) {
-		return null;
-	}
+    // Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
+    Mono<List<User>> fluxCollection(Flux<User> flux) {
+        return flux.collect(Collectors.toList());
+    }
 
 }
