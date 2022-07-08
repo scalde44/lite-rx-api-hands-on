c1c0a1b6186d2b3ce052919b18f518beacf381c1
Steven Calderon <scalderon1504@gmail.com>
2022-07-08 16:48:51 -0500

    Se soluciona el ejericio 7

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -31,35 +31,43 @@ public class Part07Errors {
 
 //========================================================================================
 
-	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
-	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
-	}
+    // Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
+    Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
+        return mono.onErrorReturn(User.SAUL);
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
-	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
-	}
+    // Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
+    Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
+        return flux
+                .onErrorResume(s -> Flux.just(User.SAUL, User.JESSE));
+    }
 
 //========================================================================================
 
-	// TODO Implement a method that capitalizes each user of the incoming flux using the
-	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    // Implement a method that capitalizes each user of the incoming flux using the
+    // #capitalizeUser method and emits an error containing a GetOutOfHereException error
+    Flux<User> capitalizeMany(Flux<User> flux) {
+        return flux
+                .concatMap(user -> {
+                    try {
+                        return Mono.just(capitalizeUser(user));
+                    } catch (GetOutOfHereException e) {
+                        return Flux.error(new GetOutOfHereException());
+                    }
+                });
+    }
 
-	User capitalizeUser(User user) throws GetOutOfHereException {
-		if (user.equals(User.SAUL)) {
-			throw new GetOutOfHereException();
-		}
-		return new User(user.getUsername(), user.getFirstname(), user.getLastname());
-	}
+    User capitalizeUser(User user) throws GetOutOfHereException {
+        if (user.equals(User.SAUL)) {
+            throw new GetOutOfHereException();
+        }
+        return new User(user.getUsername(), user.getFirstname(), user.getLastname());
+    }
 
-	protected final class GetOutOfHereException extends Exception {
-	    private static final long serialVersionUID = 0L;
-	}
+    protected final class GetOutOfHereException extends Exception {
+        private static final long serialVersionUID = 0L;
+    }
 
 }
